886e8f2177a74dd39ffcc3d4283bf873f988c786
vitordrs <soaresvitor196@gmail.com>
2025-10-03 13:34:31 -0300

    componentizando a aplicação

---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTemp' ])
+</script>
+
+<template>
+    <form @submit.prevent="props.cadastraTarefa"> 
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaTemp" @change="props.editaTarefaTemp" required type="text" placeholder="Qual sua tarefa?" class="form-control" >
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-primary" type="submit">Cadastrar</button>
+            </div>
+            <div class=" col-md-2">
+                <select @change="props.trocarFiltro" class="form-control">
+                <option value="todas">Todas tarefas</option>
+                <option value="pendentes">Tarefas pendentes</option>
+                <option value="finalizadas">Tarefas finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
